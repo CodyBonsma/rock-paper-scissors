3219b85bc7189c663eabb440233dcc79cb667fc5
Cody Bonsma <codybonsma@gmail.com>
2020-12-14 19:47:54 -0500

    added bootstrap

--- src/game/Game.jsx
+++ src/game/Game.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Game = () => {
   const [userCount, setUserCount] = useState("");
@@ -44,53 +45,33 @@ const Game = () => {
         console.log("Nothing to report");
     }
 
-    if (type === computerPlay) {
-      console.log("It's a draw");
-    } else if (type === "R" && computerPlay === "P") {
-      console.log("Paper beats rock");
-      setCompCount(compCount + 1);
-    } else if (type === "R" && computerPlay === "S") {
-      console.log("Rock beats scissors");
-      setUserCount(userCount + 1);
-    } else if (type === "P" && computerPlay === "R") {
-      console.log("Paper beats rock");
-      setUserCount(userCount + 1);
-    } else if (type === "P" && computerPlay === "S") {
-      console.log("Scissors cut paper");
-      setCompCount(compCount + 1);
-    } else if (type === "S" && computerPlay === "P") {
-      console.log("Scissors cut paper");
-      setUserCount(userCount + 1);
-    } else if (type === "S" && computerPlay === "R") {
-      console.log("Rock smashes scissors");
-      setCompCount(compCount + 1);
-    } else {
-      console.log("please choose a valid option");
-    }
+    
   };
 
   return (
     <div className="container">
       <h2>This will hold the game logic</h2>
       <div className="innerContainer">
+          <div className="row">
         <p>
           SCOREBOARD user: {userCount} vs comp: {compCount}
         </p>
       </div>
-      <div className="button rock">
+      <div className="button rock col-sm-3">
         <button value="R" onClick={(e) => gameRound(e.target.value)}>
           Rock
         </button>
       </div>
-      <div className="button paper">
+      <div className="button paper col-sm-3">
         <button value="P" onClick={(e) => gameRound(e.target.value)}>
           Paper
         </button>
       </div>
-      <div className="button scissor">
+      <div className="button scissor col-sm-3">
         <button value="S" onClick={(e) => gameRound(e.target.value)}>
           Scissors
         </button>
+      </div>
       </div>
     </div>
   );
