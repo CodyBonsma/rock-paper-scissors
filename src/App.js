14ff8c03ed5b8aaba5ce4e74d24a42ff1edf4e12
Cody Bonsma <codybonsma@gmail.com>
2020-12-31 14:42:07 -0500

    added a new homepage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Game from "./game/Game"
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Game from "./game/Game";
+import Home from "./Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <Game/>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+            <Home />
+          </Route>
+          <Route exact path={"/game"}>
+            <Game />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
