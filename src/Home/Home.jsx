8607e3ddeb19dd5ca10fca61aae3e3daaa671a5f
Cody Bonsma <codybonsma@gmail.com>
2021-01-19 20:21:47 -0500

    working on homescreen tabs

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,46 +1,44 @@
 import React from "react";
-import "./Home.css"
+import "./Home.css";
 
 const Home = () => {
   return (
-
     <div className="container main-nav">
       <h2 id="front-title">Rock Paper Scissors</h2>
-      <hr/>
+      <hr />
       <div className="card text-center home-tron">
         <div className="card-header">
-          <ul className="nav nav-tabs card-header-tabs">
+          <nav>
+          <ul className="nav nav-tabs card-header-tabs" role="tablist">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="true" href="#">
-                Active
+              <a className="nav-link active" href="#start" data-bs-toggle="tab" role="tab">
+                Start
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link disabled"
-                href="#"
-                tabIndex="-1"
-                aria-disabled="true"
-              >
-                Disabled
+              <a className="nav-link" href="#info" data-bs-toggle="tab" role="tab">
+                Info
               </a>
             </li>
           </ul>
+          </nav>
         </div>
-        <div className="card-body">
-          <h5 className="card-title">Special title treatment</h5>
-          <p className="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
-          </p>
-          <a href="/game" className="btn btn-go">
-            Start Game
-          </a>
+
+        <div className="tab-content" id="myTabContent">
+          <div className="card-body tab-pane active" id="start" role="tabpanel">
+            <h5 className="card-title">Special title treatment</h5>
+            <p className="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a href="/game" className="btn btn-go">
+              Start Game
+            </a>
+          </div>
+
+          <div className="tab-pane fade" id="info" role="tabpanel">
+            <p>this will be game info</p>
+          </div>
         </div>
       </div>
     </div>
