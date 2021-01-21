843bee92752ac9e457f0228e30b0ff1fd14933f7
Cody Bonsma <codybonsma@gmail.com>
2021-01-20 20:20:07 -0500

    back with th epain

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -7,6 +7,44 @@ const Home = () => {
       <h2 id="front-title">Rock Paper Scissors</h2>
       <hr />
 
+      <div className="card text-center">
+        <div className="card-header">
+          <ul className="nav nav-tabs card-header-tabs">
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                role="tab"
+                data-toggle="tab"
+                href="#start"
+              >
+                Active
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" role="tab" href="#info" data-toggle="tab">
+                Link
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="tab-content">
+          <div className="card-body tab-pane active" role="tabpanel" id="start">
+            <h5 className="card-title">Special title treatment</h5>
+            <p className="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+
+          <div className="card-body tab-pane" role="tabpanel" id="info">
+            <p>Hello info about the gimma</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
