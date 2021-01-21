e85bbab04ed73be53dcb0f635323ae92618fb138
Cody Bonsma <codybonsma@gmail.com>
2021-01-20 20:33:02 -0500

    using ternary for pane toggles

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { ToggleButton } from "react-bootstrap";
 import "./Home.css";
 
 const Home = () => {
+  const [pane, setPane] = useState(false);
+
+  const togglePane = () => {
+    console.log("clicked toggle");
+    setPane(true);
+  };
+
   return (
     <div className="container main-nav">
       <h2 id="front-title">Rock Paper Scissors</h2>
@@ -21,29 +29,41 @@ const Home = () => {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" role="tab" href="#info" data-toggle="tab">
+              <a
+                className="nav-link"
+                onClick={() => togglePane()}
+                role="tab"
+                href="#info"
+                data-toggle="tab"
+              >
                 Link
               </a>
             </li>
           </ul>
         </div>
 
-        <div className="tab-content">
-          <div className="card-body tab-pane active" role="tabpanel" id="start">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
-            </a>
+        {pane ? (
+          <div className="tab-content">
+            <div
+              className="card-body tab-pane active"
+              role="tabpanel"
+              id="start"
+            >
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>{" "}
           </div>
-
+        ) : (
           <div className="card-body tab-pane" role="tabpanel" id="info">
             <p>Hello info about the gimma</p>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
