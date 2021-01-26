0310e154832ee488d780983f8b5db2a527e4e721
Cody Bonsma <codybonsma@gmail.com>
2021-01-26 15:02:07 -0500

    looking at hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import "react-router";
 import Game from "./game/Game";
 import Home from "./Home/Home";
 import Footer from "./Footer/Footer";
