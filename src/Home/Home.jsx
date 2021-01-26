c2761f81fa2667415ffeb4a1555ae1bb87ee7afe
Cody Bonsma <codybonsma@gmail.com>
2021-01-26 18:06:17 -0500

    use of Link in start button

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 import { ToggleButton } from "react-bootstrap";
 import "./Home.css";
 
@@ -15,66 +16,71 @@ const Home = () => {
   };
 
   return (
-    <div className="container main-nav">
-      <h2 id="front-title">Rock, Paper, Scissors</h2>
-      <hr />
+    <BrowserRouter basename="/rock-paper-scissors">
+      <div className="container main-nav">
+        <h2 id="front-title">Rock, Paper, Scissors</h2>
+        <hr />
 
-      <div className="card text-center home-tron">
-        <div className="card-header">
-          <ul className="nav nav-tabs card-header-tabs">
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                onClick={() => toggleOptOne()}
-                role="tab"
-                data-toggle="tab"
-                href="#start"
-              >
-                Start
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                onClick={() => toggleOptTwo()}
-                role="tab"
-                href="#info"
-                data-toggle="tab"
-              >
-                Info
-              </a>
-            </li>
-          </ul>
-        </div>
+        <div className="card text-center home-tron">
+          <div className="card-header">
+            <ul className="nav nav-tabs card-header-tabs">
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  onClick={() => toggleOptOne()}
+                  role="tab"
+                  data-toggle="tab"
+                  href="#start"
+                >
+                  Start
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  onClick={() => toggleOptTwo()}
+                  role="tab"
+                  href="#info"
+                  data-toggle="tab"
+                >
+                  Info
+                </a>
+              </li>
+            </ul>
+          </div>
 
-        {pane ? (
-          <div className="tab-content">
-            <div
-              className="card-body tab-pane active"
-              role="tabpanel"
-              id="start"
-            >
-              <h4 className="card-title">Do you have the best hands on the interwebs?</h4>
-              <p className="card-text">
-                Try your moves against the computer in a classic game of rock, paper, scissors
+          {pane ? (
+            <div className="tab-content">
+              <div
+                className="card-body tab-pane active"
+                role="tabpanel"
+                id="start"
+              >
+                <h4 className="card-title">
+                  Do you have the best hands on the interwebs?
+                </h4>
+                <p className="card-text">
+                  Try your moves against the computer in a classic game of rock,
+                  paper, scissors
+                </p>
+                <Link to="/game">
+                  <button className="btn btn-go">Start Game</button>
+                </Link>
+              </div>{" "}
+            </div>
+          ) : (
+            <div className="card-body tab-pane" role="tabpanel" id="info">
+              <p>
+                Rock Paper Scissors is a zero sum game that is usually played by
+                two people using their hands. The rules to play are pretty
+                simple: Rock wins agains Scissors - Scissors wins against Paper
+                - and Paper wins against Rock. Best out of 5, wins.
               </p>
-              <a href="/game" className="btn btn-go">
-                Start Game
-              </a>
-            </div>{" "}
-          </div>
-        ) : (
-          <div className="card-body tab-pane" role="tabpanel" id="info">
-            <p>
-              Rock Paper Scissors is a zero sum game that is usually played by
-              two people using their hands. The rules to play are pretty simple:
-              Rock wins agains Scissors - Scissors wins against Paper - and
-              Paper wins against Rock. Best out of 5, wins.
-            </p>
-          </div>
-        )}
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
