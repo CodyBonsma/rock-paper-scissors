661a47c2ee732f5ec9c3834c085882ee97149dad
Cody Bonsma <codybonsma@gmail.com>
2021-01-26 18:27:13 -0500

    getting sick and tired of this bs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter basename="/rock-paper-scissors">
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
