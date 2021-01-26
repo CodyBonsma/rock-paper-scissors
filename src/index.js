127a1269adca7801164361f42baf7e4ac02cd240
Cody Bonsma <codybonsma@gmail.com>
2021-01-26 16:02:31 -0500

    forgot basename

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BrowserRouter from "react-router-dom";
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <BrowserRouter basename="/rock-paper-scissors">
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
